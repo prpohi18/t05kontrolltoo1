6eb535426b05a608f1605a13aa81f11e930764b8
Sergei Frolov <serfro@tlu.ee>
2018-03-01 12:18:24 +0200

    KT1

---
+++ src/main/java/ee/tlu/sergei/kt1/Andmed.java
@@ -0,0 +1,49 @@
+
+package ee.tlu.sergei.kt1;
+
+public class Andmed {
+    private double k;
+    private double t0;
+    private double t6;
+    private double t12;
+    private double t18;
+    private double t24;
+    
+    public void setKuupaev(double k){
+        this.k = k;
+    }
+    public void setTemp0(double t0){
+        this.t0 = t0;
+    }
+    public void setTemp6(double t6){
+        this.t6 = t6;
+    }
+    public void setTemp12(double t12){
+        this.t12 = t12;
+    }
+    public void setTemp18(double t18){
+        this.t18 = t18;
+    }
+    public void setTemp24(double t24){
+        this.t24 = t24;
+    }
+    
+    public double getKuupaev(){
+        return k;
+    }
+    public double getTemp0(){
+        return t0;
+    }
+    public double getTemp6(){
+        return t6;
+    }
+    public double getTemp12(){
+        return t12;
+    }
+    public double getTemp18(){
+        return t18;
+    }
+    public double getTemp24(){
+        return t24;
+    }
+}
--- src/main/java/ee/tlu/sergei/kt1/KroonoloogilineKeskmine.java
+++ src/main/java/ee/tlu/sergei/kt1/KroonoloogilineKeskmine.java
@@ -1,6 +1,16 @@
 
 package ee.tlu.sergei.kt1;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class KroonoloogilineKeskmine {
 
     /*public static void main(String[] args) {
@@ -43,8 +53,49 @@ public class KroonoloogilineKeskmine {
     }
 
     public static void main(String[] args) {
-        
+        try{
+            String filePath = "C:\\Users\\user\\Desktop\\TLU\\Semester 2\\ProgPohikursus\\temp";
+            BufferedReader br = new BufferedReader(new FileReader(filePath  + ".txt"));
+            String rida;
+            
+            List<Andmed> andmed = new ArrayList<>();
+            int ridaCount = 0;
+            while((rida = br.readLine()) != null){
+                String[] temp = rida.split(",");
+                Andmed andmed0 = new Andmed();
+                andmed0.setKuupaev(Double.valueOf(temp[0]));
+                andmed0.setTemp0(Double.valueOf(temp[1]));
+                andmed0.setTemp6(Double.valueOf(temp[2]));
+                andmed0.setTemp12(Double.valueOf(temp[3]));
+                andmed0.setTemp18(Double.valueOf(temp[4]));
+                andmed0.setTemp24(Double.valueOf(temp[5]));
+                
+                andmed.add(andmed0);
+                ridaCount++;
+                
+            }
+            
+            br.close();
+            File fout = new File(filePath + "_avgtemp"  + ".txt");
+            FileOutputStream fos = new FileOutputStream(fout);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+            Andmed andmed0 = new Andmed();
+            
+            for (int i = 0; i < ridaCount; i++) {
+                
+                double[] andmed1 = {andmed0.getTemp0(), andmed0.getTemp12(), andmed0.getTemp18(), andmed0.getTemp24()};
+                double[] andmedFaili = {andmed0.getKuupaev(), KronKesk2(andmed1)};
+                bw.write(Arrays.toString(andmedFaili) + "\n");
+            }
+            bw.close();
+            
+        } catch(Exception ex){
+            ex.printStackTrace();
+        }
     
     }
+
+
+
     
 }
