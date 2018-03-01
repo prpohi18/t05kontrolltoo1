fbe8c1c9fea46ca9e437069df8dcb48531d0bb9d
Egert Klaamas <31847333+egert112@users.noreply.github.com>
2018-03-01 12:16:50 +0200

    Add files via upload

---
+++ Kontrolltoo1,2/src/kontrolltoo1/pkg2/Ülessanne1.java
@@ -0,0 +1,20 @@
+//Kronoloogilise keskmise KT
+package kontrolltoo1.pkg2;
+
+
+public class Ülessanne1 {
+    
+  
+
+ 
+    public static void main(String[] args) {
+        double k;
+        double a = 5.0/2;
+        int b = 5;
+        double c = 5.0/2;
+        k = (a+b+c)/2;
+        System.out.println(k);
+    }
+    
+}
+//5.0
