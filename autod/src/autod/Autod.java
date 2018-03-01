8545c6eca2a7af063c73a0e1dca48a3cf75513af
anuxyo <anuusadam@gmail.com>
2018-03-01 09:37:01 +0200

    1. punkt tehtud

--- autod/src/autod/Autod.java
+++ autod/src/autod/Autod.java
@@ -9,8 +9,12 @@ public class Autod {
     
     
     public static void main(String[] args) {
-        
-        
+        Autod2 auto1=new Autod2(500, 60);
+        Autod2 auto2=new Autod2(650, 55);
+        System.out.println("Esimese auto kaugus on "+auto1.s +" km "
+                + "ning tema kiirus on "+auto1.v+" km/h");
+        System.out.println("Teise auto kaugus on "+auto2.s +" km "
+                + "ning tema kiirus on "+auto2.v+" km/h");
     }
     
 }
---
+++ autod/src/autod/Autod2.java
@@ -0,0 +1,18 @@
+package autod;
+/**
+ *
+ * @author Anu
+ */
+public class Autod2 {
+   
+    double s, v;
+    
+    public Autod2(double kaugus, double kiirus){
+        s=kaugus;
+        v=kiirus;
+        
+    }
+    
+    
+    
+}
